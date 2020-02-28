aa1eb11a82462b804a8320bc751eacc98ad368cd
akaregi <akg.tachibana@gmail.com>
2020-02-29 06:55:42 +0900

    Added method and member documents
    Signed-off-by: akaregi <akg.tachibana@gmail.com>

--- src/Dice.ts
+++ src/Dice.ts
@@ -1,39 +1,67 @@
+/**
+ * ダイス式(e.g. `xDn<=C`)のインタフェース
+ */
 export interface DiceFormula {
   definition: DiceDefinition,
   condition: DiceCondition
 }
 
+/**
+ * ダイス式のうちダイス定義(`xDn<=C` の `xDn`)のインタフェース
+ */
 export interface DiceDefinition {
-  // ダイスの量
-  // AdB の A
+  /**
+   * ダイスの量(`xDn` の `x`)
+   */
   amount: Number
 
-  // ダイスの面の数
-  // AdB の B
+  /**
+   * ダイスの面の数(`xDn` の `n`)
+   */
   faces: Number
 }
 
+/**
+ * ダイス式のうち条件部分(`xDn<=C` の `<=C`)のインタフェース
+ */
 export interface DiceCondition {
-  // ダイスの条件オペレータ e.g.  1d100<=30 の <=
+  /**
+   * ダイスの条件オペレータ(`xDn<=C` の `<=`)
+   */
   operator: Operator,
 
-  // ダイスの条件オペランド e.g. 1d100<=30 の 30
+  /**
+   * ダイスの条件オペランド(`xDn<=C` の `C`)
+   */
   operand: Number
 }
 
+/**
+ * ダイス条件式のオペレータ
+ */
 export enum Operator {
-  // 1d100>10, X > Y
+  /**
+   * X > Y
+   */
   BIGGER,
 
-  // 1d100<10; X < Y
+  /**
+   * X < Y
+   */
   SMALLER,
 
-  // 1d100>=10; X >= Y
+  /**
+   * X >= Y
+   */
   BIGGER_OR_EQUAL,
 
-  // 1d100<=10; X <= Y
+  /**
+   * X <= Y
+   */
   SMALLER_OR_EQUAL,
 
-  // 1d100=10; X = Y
+  /**
+   * X = Y
+   */
   EQUAL
 }
--- src/RNG.ts
+++ src/RNG.ts
@@ -1,12 +1,28 @@
 import { RandomGenerator, Distribution, xorshift128plus, uniformIntDistribution } from 'pure-rand'
 
+/**
+ * ランダムな整数値を出力する乱数装置
+ */
 export class RandomNumberGenerator {
+  // NOTE: xorshift で決め打ち
   private xorshift: RandomGenerator
   private distribution: Distribution<number>
 
+  /**
+   * 現在の乱数
+   */
   private N: Number
-  private nextGen: RandomGenerator
 
+  /**
+   * 現在の乱数装置
+   */
+  private generator: RandomGenerator
+
+  /**
+   * @param seed シード値
+   * @param min 乱数の最小値
+   * @param max 乱数の最大値
+   */
   constructor (seed: number, min: number, max: number) {
     this.xorshift = xorshift128plus(seed)
     this.distribution = uniformIntDistribution(min, max)
@@ -14,22 +30,32 @@ export class RandomNumberGenerator {
     const initGen = this.distribution(this.xorshift)
 
     this.N = initGen[0]
-    this.nextGen = initGen[1]
+    this.generator = initGen[1]
   }
 
+  /**
+   * 現在の乱数を返して乱数装置を次の状態に進める
+   *
+   * @return 現在の乱数
+   */
   next (): Number {
     const next = this.renew()
 
-    this.nextGen = next.generator
+    this.generator = next.generator
     this.N = next.n
 
     return this.N
   }
 
+  /**
+   * 乱数装置を次の状態に進める
+   */
   private renew () {
-    const next = this.distribution(this.nextGen)
+    const next = this.distribution(this.generator)
 
     return {
+      // NOTE: pure-rand の都合でインデックスでアクセスする
+      //       [0] が次の乱数 [1] が状態が進んだ乱数装置
       n: next[0],
       generator: next[1]
     }
